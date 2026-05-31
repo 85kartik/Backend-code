e2ea1442283a8b30163bf1d1476a5d1cbdd3290e
karteek Parab <kartikparab887@gmail.com>
2026-05-31 10:33:52 +0530

    add server file

---
+++ routes/server.js
@@ -0,0 +1,6 @@
+const express = require('express')
+dotenv.config({path:"./routes/.env"});
+const app = express()
+const port = process.env.PORT
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
